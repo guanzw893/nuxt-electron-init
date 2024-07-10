59ef10fa6ae69430edd67bd604a1681ec07aba0f
Zhiwei Guan <Zhiwei.Guan@axicorp.com>
2024-07-10 18:36:05 +0800

    ✨ feat(main): 添加托盘菜单

--- electron/main.ts
+++ electron/main.ts
@@ -1,12 +1,40 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Tray, Menu, nativeImage } from 'electron'
 
-app.whenReady().then(() => {
-  const win = new BrowserWindow({
+import iconPath from '../assets/image/logo.jpg'
+
+const createMainWindow = () => {
+  return new BrowserWindow({
     title: 'Main window',
     autoHideMenuBar: true,
     hiddenInMissionControl: true,
     minWidth: 720
   })
+}
+
+const createMenu = (win: BrowserWindow) => {
+  const icon = nativeImage.createFromDataURL(iconPath)
+  const tray = new Tray(icon)
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '退出',
+      click() {
+        win.destroy()
+        app.quit()
+      }
+    }
+  ])
+
+  tray.setToolTip('Tool Tip')
+  tray.setContextMenu(contextMenu)
+  tray.on('click', () => {
+    win.show()
+  })
+}
+
+app.whenReady().then(() => {
+  const win = createMainWindow()
+  createMenu(win)
 
   if (!app.isPackaged && process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL)
